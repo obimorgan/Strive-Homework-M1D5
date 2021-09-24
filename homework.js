db108f2defc878ac9fa52bb01237bf0780bc9c7c
Roby Morgan <roby.c.morgan@gmail.com>
2021-09-24 15:50:53 +0200

    Completed first few exercises.

--- homework.js
+++ homework.js
@@ -11,54 +11,93 @@ ASSIGNMENT RULES
 /* EXERCISE 1
  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
-
-/* WRITE YOUR ANSWER HERE */
+const area = function (l1, l2) {
+    return l1 * l2
+}
 
 /* EXERCISE 2
  Write a function called "crazySum" which receives two integers as parameters.
  It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const crazySum = function(num1,num2) {
+    if (num1 !== num2) {
+        num1 + num2
+    } else {
+        return (num1 + num2) * 3
+    }
+}
 
 /* EXERCISE 3
  Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
  It should return triple their absolute difference if the given number is greater than 19.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const crazyDiff = function(num) {
+    if (num > 19) {
+        return(num - 19) * 3
+    } else {
+        return num - 19
+    }
+}
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const boundary = function (n) {
+    if ((n > 20 && n <= 100) || n === 400) {
+        return true
+    } else {
+        return false
+    }
+}
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const strivify = function (str) {
+    if (str !== "Strive") {
+        console.log(`"Strive ${str}`)
+    } else {
+        console.log(str)
+    }
+}
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
 */
 
-/* WRITE YOUR ANSWER HERE */
+const check3and7 = function (possitiveNumber) {
+    if (possitiveNumber % 3 === 0 || possitiveNumber % 7 === 0) {
+        console.log("This is a possitive number!")
+    }
+} 
 
 /* EXERCISE 7
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
+const reverseString = function(str) {
+    let newString = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
 
-/* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const upperFirst = function(str) {
+
+}
+
+
 
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
